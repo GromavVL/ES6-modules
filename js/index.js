f22bd227a3307f265696af0fd45d77c992228beb
Vlad <vlad086367@gmail.com>
2025-09-07 22:58:19 +0300

    Add ES6 model

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// іменований імпорт -----------------
+// import { sum as mySum, mult, PI } from './math.js';
+
+// console.log('sum(1,2) :>> ', mySum(1, 2));
+// console.log('mult(PI,2) :>> ', mult(PI, 2));
+
+// або
+// import * as MyMath from './math.js';
+
+// console.log('MyMath.sum(PI,3) :>> ', MyMath.sum(MyMath.PI, 3));
+
+// дефолтний імпорт -----------------------
---
+++ js/math.js
@@ -0,0 +1,15 @@
+// іменований експорт ----------------
+
+export const sum = (a, b) => a + b;
+
+export const mult = (a, b) => a * b;
+
+export const PI = 3.14;
+
+// або
+// const sum = (a, b) => a + b;
+// const mult = (a, b) => a * b;
+// const PI = 3.14;
+// export { sum, mult, PI };
+
+//  дефолтний експорт --------------
