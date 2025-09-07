bbf5d2fae78b92fa6dbfabb257db8d819ea6b78c
Vlad <vlad086367@gmail.com>
2025-09-07 23:10:50 +0300

    Import and export

--- js/index.js
+++ js/index.js
@@ -10,3 +10,14 @@
 // console.log('MyMath.sum(PI,3) :>> ', MyMath.sum(MyMath.PI, 3));
 
 // дефолтний імпорт -----------------------
+import Test  from "./math.js";
+
+console.log('new Test(5) :>> ', new Test(5));
+
+// поэднаний варіант -------------------------
+// import { default as MyTest, sum, mult, PI } from './math.js';
+
+import MyTest, { sum, mult, PI } from './math.js';
+
+console.log('mult(PI,2) :>> ', mult(PI, 2));
+console.log('new MyTest(5) :>> ', new MyTest(5));
--- js/math.js
+++ js/math.js
@@ -13,3 +13,18 @@ export const PI = 3.14;
 // export { sum, mult, PI };
 
 //  дефолтний експорт --------------
+class Test {
+    constructor(tmp) {
+        this.tmp = tmp;
+    }
+}
+
+export default Test;
+
+// все експортоване потрапляє ніби в об'єкт
+// {
+//   sum: (a, b) => a + b,
+//   mult: (a, b) => a * b,
+//   PI: 3.14
+//   default: Test
+// }
